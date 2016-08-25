8e1ba2bb52e6b528abcdd4a0e6c6d907b2c9edf3
zo7 <zo7.flynn@gmail.com>
2016-08-25 15:54:42 -0400

    Add command to preprocess images

--- painter.py
+++ painter.py
@@ -15,11 +15,65 @@ import argparse
 import os
 import sys
 import types
+import warnings
 
+from PIL import Image
+from tqdm import tqdm
+
+warnings.simplefilter('ignore', Image.DecompressionBombWarning)
 sys.path.append('deep-learning-models')
 
 
 # ---- Available commands
+
+def preprocess():
+    """
+    Command to preprocess a dataset. Resizes images such that their smallest
+    size is fixed to some value.
+
+    Needed because some of these images are huuuge and processing/loading them
+    online becomes a bottleneck.
+    """
+
+    parser = argparse.ArgumentParser(
+            description = "Preprocesses a dataset.")
+    parser.add_argument('data', type=str, help=
+            "Directory where data lives.")
+    parser.add_argument('output', type=str, help=
+            "Directory to write files to.")
+    parser.add_argument('-s', '--size', type=int, default=256, help=
+            "Size to constrain the smallest side to.")
+
+    args = parser.parse_args(sys.argv[2:])
+
+    all_files = [x for x in os.listdir(args.data)
+                 if os.path.isfile( os.path.join(args.data, x) )]
+
+    if not os.path.exists(args.output):
+        os.makedirs(args.output)
+
+    print("Processing '{}' -> '{}' with base size {}".format(
+        args.data, args.output, args.size))
+
+    for image_filename in tqdm(all_files):
+        img = Image.open( os.path.join(args.data, image_filename) )
+
+        w, h = img.size
+        if w < h:
+            new_w = args.size
+            new_h = int(h * (args.size/float(w)))
+        else:
+            new_w = int(w * (args.size/float(h)))
+            new_h = args.size
+
+        try:
+            img.resize((new_w, new_h))\
+               .convert('RGB')\
+               .save( os.path.join(args.output, image_filename) )
+        except Exception as e:
+            print("Unable to process {}".format(image_filename))
+            print(e)
+
 
 def train():
     """
@@ -44,7 +98,13 @@ def train():
 
     args = parser.parse_args(sys.argv[2:])
 
-    raise RuntimeError("Not implemented yet!")
+    import painter
+
+    painter.train(args.data, args.model,
+        batch_size       = args.batch_size,
+        num_epochs       = args.num_epochs,
+        patience         = args.patience,
+    )
 
 
 def test():
@@ -60,6 +120,8 @@ def test():
             "Output .csv file to write to.")
 
     args = parser.parse_args(sys.argv[2:])
+
+    import painter
 
     raise RuntimeError("Not implemented yet!")
 
